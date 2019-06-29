65d3b641a8577a3be2edaea8fc2c3dc13ecdf39e
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-06-29 14:53:19 -0400

    Haciendo las Entidades

---
+++ Parcial2-Adriel/Entidades/Estudiantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_Adriel.Entidades
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+
+        public DateTime FechaIngresos { get; set; }
+        public string Nombres { get; set; }
+
+        public int Balance { get; set; }
+
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            FechaIngresos = DateTime.Now;
+            Nombres = string.Empty;
+            Balance = 0;
+        }
+    }
+}
---
+++ Parcial2-Adriel/Entidades/Inscripcion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_Adriel.Entidades
+{
+    public class Inscripcion
+    {
+        [Key]
+
+        public int InscripcionId { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public int Monto { get; set; }
+
+        public Inscripcion()
+        {
+            InscripcionId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+        }
+    }
+}
