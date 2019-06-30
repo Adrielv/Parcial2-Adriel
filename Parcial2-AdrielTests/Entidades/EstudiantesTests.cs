822d85a3055c51ba3076ad1c54021a9eac1101a9
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-06-30 01:12:37 -0400

    Finalizando test Estudiante

--- Parcial2-AdrielTests/Entidades/EstudiantesTests.cs
+++ Parcial2-AdrielTests/Entidades/EstudiantesTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_Adriel.BLL;
 using Parcial2_Adriel.Entidades;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,54 @@ namespace Parcial2_Adriel.Entidades.Tests
     public class EstudiantesTests
     {
         [TestMethod()]
-        
+        public void EstudianteGuardarTest()
+        {
+            Estudiantes e = new Estudiantes();
+            e.EstudianteId = 1;
+            e.FechaIngresos = DateTime.Now;
+            e.Nombres = "Adriel";
+            e.Balance = 1000;
+
+            RepositorioBase<Estudiantes> r = new RepositorioBase<Estudiantes>();
+            bool paso = false;
+            paso = r.Guardar(e);
+            Assert.AreEqual(true, paso);
+        }
+        [TestMethod()]
+        public void EstudianteModificarTest()
+        {
+            RepositorioBase<Estudiantes> repositorio = new RepositorioBase<Estudiantes>();
+            bool paso = false;
+            Estudiantes e = repositorio.Buscar(1);
+            e.Nombres = "Pedro";
+            paso = repositorio.Modificar(e);
+            Assert.AreEqual(true, paso);
+        }
+        [TestMethod()]
+        public void EstudianteBuscarTest()
+        {
+            RepositorioBase<Estudiantes> repositoriobase = new RepositorioBase<Estudiantes>();
+            Estudiantes e = repositoriobase.Buscar(1);
+            Assert.IsNotNull(e);
+        }
+
+        [TestMethod()]
+        public void EstudianteGetListTest()
+        {
+            RepositorioBase<Estudiantes> repositorio = new RepositorioBase<Estudiantes>();
+            List<Estudiantes> lista = new List<Estudiantes>();
+            lista = repositorio.GetList(e => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EstudiantesEliminarTest()
+        {
+            RepositorioBase<Estudiantes> repositoriobase = new RepositorioBase<Estudiantes>();
+            bool paso = false;
+            paso = repositoriobase.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+
     }
 }
